c73ee7bcab373343af4ba0d0233e0000e5ba4785
Vianney Rancurel <vianney.rancurel@scality.com>
2017-05-30 14:37:57 -0700

    fft computations passes

--- src/fft.h
+++ src/fft.h
@@ -6,18 +6,18 @@ template<typename T>
 class FFT
 {
  private:
+  GF<T> *gf;
   T omega;
   int q;
   int N;
   Vec<T> *W;
   void compute_omegas(void);
  public:
-  GF<T> *gf;
   FFT(GF<T> *gf, T omega, int q);
   ~FFT();
   int _get_p(int i, int j);
-  int _get_p0(int i, int j, int x);
-  int _get_p1(int i, int j, int x);
+  int _get_p0(int i, int j, int s);
+  int _get_p1(int i, int j, int s);
   void fft(Vec<T> *output, Vec<T> *input);
 };
 
