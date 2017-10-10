ccb0c24f768f17589775e233a4f9786bbeece3fb
Lam Pham-Sy <lam.pham-sy@scality.com>
2017-10-10 02:01:57 +0200

    Vec: constructor with mem and mem length & copy
    Vector constructor has two new parameters:
    - mem (=NULL by default): an array pointer. If it's given, instance of
      vector's `mem` is actually this pointer.
    - mem_len (=0 by default): length of `mem`
    
    Do copy on `mem` array
    - copy(vec): do copy vec's `mem` to this vector
    - copy(vec, n): do copy vec's `mem` to this vector; zero out next
      (n-vec's `mem_len`) elements of the vector's `mem`
      if n > vec's `mem_len`
    
    vec: copy n

--- src/vec.h
+++ src/vec.h
@@ -5,20 +5,20 @@ template<typename T>
 class Poly;
 
 template<typename T>
-class VmVec;
-
-template<typename T>
 class Vec
 {
  private:
   T *mem;
+  int mem_len;
+  bool new_mem;
  protected:
   int n;
  public:
   MG<T> *mg;
-  Vec(MG<T> *mg, int n);
+  Vec(MG<T> *mg, int n, T* mem=NULL, int mem_len=0);
   ~Vec();
   virtual int get_n(void);
+  int get_mem_len(void);
   void zero_fill(void);
   virtual void set(int i, T val);
   virtual T get(int i);
@@ -33,31 +33,43 @@ class Vec
   void add_mutual(Vec<T> *v, int offset, int len);
   void copy(Vec<T> *v);
   void copy(Vec<T> *v, int n);
-  void copy_mutual(Vec<T> *v);
-  void copy_mutual(Vec<T> *v, int offset);
   bool eq(Vec<T> *v);
   void to_poly(Poly<T> *poly);
-  void dump(void);
+  virtual void dump(void);
 };
 
 template <typename T>
-Vec<T>::Vec(MG<T> *mg, int n)
+Vec<T>::Vec(MG<T> *mg, int n, T* mem, int mem_len)
 {
   this->mg = mg;
   this->n = n;
-  this->mem = new T[n];
+  if (mem == NULL) {
+    this->mem = new T[n];
+    this->mem_len = n;
+    this->new_mem = true;
+  } else {
+    this->mem = mem;
+    this->mem_len = mem_len;
+    this->new_mem = false;
+  }
 }
 
 template <typename T>
 Vec<T>::~Vec()
 {
-  delete[] this->mem;
+  if (new_mem) delete[] this->mem;
 }
 
 template <typename T>
 int Vec<T>::get_n(void)
 {
   return n;
+}
+
+template <typename T>
+int Vec<T>::get_mem_len(void)
+{
+  return this->mem_len;
 }
 
 template <typename T>
@@ -188,31 +200,21 @@ void Vec<uint64_t>::add(Vec<uint64_t> *v);
 template <typename T>
 void Vec<T>::copy(Vec<T> *v)
 {
-  assert(v->get_n() == this->n);
-
-  std::copy_n(v->mem, v->get_n(), this->mem);
+  assert(v->get_mem_len() <= this->mem_len);
+  std::copy_n(v->get_mem(), v->get_mem_len(), this->mem);
 }
 
 template <typename T>
 void Vec<T>::copy(Vec<T> *v, int n)
 {
-  assert(n <= this->n);
-
-  std::copy_n(v->mem, n, this->mem);
-}
-
-template <typename T>
-void Vec<T>::copy_mutual(Vec<T> *v)
-{
-  assert(n >= v->get_n());
-  std::copy_n(v->mem, v->get_n(), this->mem);
-}
-
-template <typename T>
-void Vec<T>::copy_mutual(Vec<T> *v, int offset)
-{
-  assert(n - offset >= v->get_n());
-  std::copy_n(v->mem, v->get_n(), this->mem + offset);
+  assert(n <= this->mem_len);
+  int v_mem_len = v->get_mem_len();
+  if (v_mem_len >= n)
+    std::copy_n(v->get_mem(), n, this->mem);
+  else {
+    std::copy_n(v->get_mem(), v_mem_len, this->mem);
+    std::memset(this->mem + v_mem_len, 0, sizeof(T) * (n - v_mem_len));
+  }
 }
 
 template <typename T>
