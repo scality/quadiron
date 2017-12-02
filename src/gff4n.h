8a213fb0809c38b8085033cc7c3b3fbb2696439c
Lam Pham-Sy <lam.pham-sy@scality.com>
2017-12-02 06:32:35 +0100

    GFF4N: add new functions
    - pack:
        - pack tuple of 16-bit numbers to tuple of 32-bit numbers
        - pack complex number to tuple of 32-bit numbers
    - unpack: unpack tuple of 32-bits numbers to complex number
    - weak_rand
    etc.

--- src/gff4n.h
+++ src/gff4n.h
@@ -14,7 +14,7 @@ private:
   T unit;
   T q;
   T h;
-  GF<T> *sub_field;
+  GF<uint32_t> *sub_field;
   bool check_n(int n);
   void init(void);
 
@@ -23,6 +23,7 @@ public:
   ~GFF4N();
   T card(void);
   T card_minus_one(void);
+  bool check(T a);
   T neg(T a);
   T add(T a, T b);
   T sub(T a, T b);
@@ -31,14 +32,22 @@ public:
   T inv(T a);
   T exp(T a, T b);
   T log(T a, T b);
+  T weak_rand_tuple(void);
   T weak_rand(void);
   T get_unit(void);
+  T replicate(T a);
+  T pack(T a);
+  T pack(T a, uint32_t flag);
+  compT<T> unpack(T a);
+  T get_nth_root(T n);
+  void compute_omegas(Vec<T> *W, int n, T w);
+  GF<uint32_t> *get_sub_field();
 };
 
 template <typename T>
 GFF4N<T>::GFF4N(int n) : GF<T>(65537, n)
 {
-  sub_field = new GFP<T>(65537);
+  sub_field = new GFP<uint32_t>(65537);
 
   if (!check_n(n)) {
     // not supported yet
@@ -68,19 +77,23 @@ bool GFF4N<T>::check_n(int n)
 }
 
 template <typename T>
-void GFF4N<T>::init(void)
+T GFF4N<T>::replicate(T a)
 {
-  unit = 1;
-  q = 65537;
-  h = 65536;
+  T b = a;
   for (int i = 1; i < this->n; i++) {
-    unit += (unit << 32);
-    q += (q << 32);
-    h += (h << 32);
+    b += (a <<= 32);
   }
+  return b;
 }
 
-// FIXME: n >= sizeof(T) / sizeof(F4)
+template <typename T>
+void GFF4N<T>::init(void)
+{
+  unit = replicate(1);
+  q = replicate(65537);
+  h = replicate(65536);
+}
+
 template <typename T>
 T GFF4N<T>::card(void)
 {
@@ -224,7 +237,7 @@ T GFF4N<T>::log(T a, T b)
 }
 
 template <typename T>
-T GFF4N<T>::weak_rand(void)
+T GFF4N<T>::weak_rand_tuple(void)
 {
   T c = 0;
   uint32_t mask = 0xffffffff;
@@ -236,4 +249,112 @@ T GFF4N<T>::weak_rand(void)
     shift += 32;
   }
   return c;
+}
+
+template <typename T>
+T GFF4N<T>::weak_rand(void)
+{
+  T c = weak_rand_tuple();
+  return unpack(c).val;
+}
+
+/**
+ * Pack of n numbers each of 16 bits into n numbers each of 32 bits
+ */
+template <typename T>
+T GFF4N<T>::pack(T a)
+{
+  uint32_t mask = 0xffff;
+  int shift = 0;
+  T b = 0;
+  for (int i = 0; i < this->n; i++) {
+    b += ((a & mask) << shift);
+    shift += 32;
+    a >>= 16;
+  }
+  return b;
+}
+
+/**
+ * Pack of n numbers each of 16 bits into n numbers each of 32 bits
+ *  If flag contains 2^i, ith number == 65537
+ */
+template <typename T>
+T GFF4N<T>::pack(T a, uint32_t flag)
+{
+  uint32_t mask = 0xffff;
+  int shift = 0;
+  T b = 0;
+  for (int i = 0; i < this->n; i++) {
+    if (flag % 2 == 1) {
+      b += ((T)65536 << shift);
+    } else {
+      b += ((a & mask) << shift);
+    }
+    flag >>= 1;
+    shift += 32;
+    a >>= 16;
+  }
+  return b;
+}
+
+
+/**
+ * Unpack of n numbers each of 32 bits into n numbers each of 16 bits
+ *  If ith number == 65537, add 2^i to flag to mark it, and consider this number
+ *  as zero
+ */
+template <typename T>
+compT<T> GFF4N<T>::unpack(T a)
+{
+  compT<T> b = compT<T>();
+  uint32_t mask = 0xffffffff;
+  int shift = 0;
+  for (int i = 0; i < this->n; i++) {
+    T tmp = (a & mask);
+    if (tmp == 65536) {
+      b.flag += (1 << i);
+    } else {
+      b.val += (tmp << shift);
+    }
+    shift += 16;
+    a >>= 32;
+  }
+  return b;
+}
+
+// Use for fft
+template <typename T>
+T GFF4N<T>::get_nth_root(T n)
+{
+  T sub_w = sub_field->get_nth_root(n);
+  T w = replicate(sub_w);
+  return w;
+}
+
+template <typename T>
+bool GFF4N<T>::check(T a)
+{
+  return (a >= 0 && a < q);
+}
+
+/**
+ * Compute the different powers of the root of unity into a vector
+ *
+ * @param W output vector (must be of length n+1)
+ * @param n
+ * @param w n-th root of unity
+ */
+template <typename T>
+void GFF4N<T>::compute_omegas(Vec<T> *W, int n, T w)
+{
+  for (int i = 0; i <= n; i++) {
+    W->set(i, this->exp(w, replicate(i)));
+  }
+}
+
+template <typename T>
+GF<uint32_t> *GFF4N<T>::get_sub_field()
+{
+  return sub_field;
 }
