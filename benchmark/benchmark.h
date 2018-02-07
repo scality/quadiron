scality/quadiron

commit 8daf6a0f9980b85c8a09fea0f7015f5afe350dbe

V2Vecp: virtual double length Vecp

src/v2vecp.h
@@ -1,0 +1,44 @@
+/* -*- mode: c++ -*- */
+#pragma once
+
+/**
+ * Virtual (v->get_n()*2) x 1 vertical vector for the need of
+ * Cooley-Tukey algorithm
+ *
+ * | v_0 |
+ * | v_1 |
+ * | ...
+ * | v_b |
+ * | v_0 |
+ * | v_1 |
+ * | ...
+ * | v_b |
+ */
+template<typename T>
+class V2Vecp : public Vecp<T>
+{
+ private:
+  Vecp<T> *vec;
+ public:
+  explicit V2Vecp(Vecp<T> *vec);
+  T* get(int i);
+};
+
+template <typename T>
+V2Vecp<T>::V2Vecp(Vecp<T> *vec) :
+  Vecp<T>(2*vec->get_n(), vec->get_size(), vec->get_mem())
+{
+  this->vec = vec;
+}
+
+template <typename T>
+T* V2Vecp<T>::get(int i)
+{
+  int vec_n = vec->get_n();
+  assert(i >= 0 && i < 2*vec_n);
+
+  if (i < vec_n)
+    return vec->get(i);
+  else
+    return vec->get(i - vec_n);
+}

src/ntl.h
@@ -58,6 +58,7 @@ typedef enum
 #include "v2vec.h"
 #include "vecp.h"
 #include "vvecp.h"
+#include "v2vecp.h"
 #include "vcvec.h"
 #include "vmvec.h"
 #include "mat.h"

commit f8329aae2aaa152b82f250da8d7c3a18b64d6869

Add missing include guard
Lint errors were introduced in 8daf6a0f9980 and fixed in 97e96a27bda1.
Unfortunately, include guard were missing.

This commit fixes this issue.

benchmark/benchmark.h
@@ -1,4 +1,6 @@
 /* -*- mode: c++ -*- */
+#ifndef __NTL_NETL_BENCH_BENCHMARK_H__
+#define __NTL_NETL_BENCH_BENCHMARK_H__
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -320,3 +322,5 @@ class Benchmark
   void show(Stats_t *stats);
   void show(Stats_t *stats1, Stats_t *stats2);
 };
+
+#endif

src/v2vecp.h
@@ -1,4 +1,6 @@
 /* -*- mode: c++ -*- */
+#ifndef __NTL_V2VECP_H__
+#define __NTL_V2VECP_H__
 
 #include "vecp.h"
 
@@ -43,3 +45,5 @@ T* V2Vecp<T>::get(int i)
   else
     return vec->get(i - vec_n);
 }
+
+#endif