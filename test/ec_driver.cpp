74f45b386b01fd918196758df38757bb238bb8dd
Lam Pham-Sy <lam.pham-sy@scality.com>
2018-06-13 03:39:10 +0200

    EC driver: RS-NF4 using packets

--- test/ec_driver.cpp
+++ test/ec_driver.cpp
@@ -360,7 +360,8 @@ template <typename T>
 void run_fec_rs_nf4(int word_size, int n_data, int n_parities, int rflag)
 {
     nttec::fec::RsNf4<T>* fec;
-    fec = new nttec::fec::RsNf4<T>(word_size, n_data, n_parities);
+    size_t pkt_size = 1024;
+    fec = new nttec::fec::RsNf4<T>(word_size, n_data, n_parities, pkt_size);
 
     if (tflag) {
         print_fec_type<T>(fec);
