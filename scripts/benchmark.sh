4acda30d1e050ec7a1ea13af194eb96a7b3c6e34
Lam Pham-Sy <lam.pham-sy@scality.com>
2018-06-21 01:35:51 +0200

    Benchmark scenarios: run only for fnt

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -43,38 +43,14 @@ then
 fi
 
 chunk_size=51200
-# for rs-nf4
-ec_type=rs-nf4
-word_size=8
-for k in 16; do
-  for n in 256 1024; do
-    m=$((n-k))
-    ${bin} -e ${ec_type} -w ${word_size} -k ${k} -m ${m} -c ${chunk_size} -s ${sce_type} -g ${threads_nb} -f ${show_type}
-    show_type=0
-  done
-done
-
 # for rs-fnt with different packet sizes
 ec_type=rs-fnt
 word_size=2
 for k in 16; do
   for n in 256 1024; do
     m=$((n-k))
-    for pkt_size in 0 256 512; do
+    for pkt_size in 256 512 1024; do
       ${bin} -e ${ec_type} -w ${word_size} -k ${k} -m ${m} -c ${chunk_size} -s ${sce_type} -g ${threads_nb} -f ${show_type} -p ${pkt_size}
-    done
-  done
-done
-
-# for all cases
-sce_type=enc_dec
-chunk_size=512
-for ec_type in rs-gf2n-v rs-gf2n-c rs-gf2n-fft rs-gf2n-fft-add rs-gfp-fft rs-fnt rs-nf4; do
-  for k in 5; do
-    for m in 2; do
-      for word_size in 1 2 4 8; do
-        ${bin} -e ${ec_type} -w ${word_size} -k ${k} -m ${m} -c ${chunk_size} -s ${sce_type} -g ${threads_nb} -f ${show_type}
-      done
     done
   done
 done
