2e76624aabeee118115668ee5d5a5fbe767de298
Lam Pham-Sy <lam.pham-sy@scality.com>
2018-06-26 16:32:40 +0200

    Bench LEO: bash to get leopard

---
+++ benchmark/libs/get_leo.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+#leo
+if [ -d ./leopard/.git ]; then
+    pushd leopard
+    git pull
+    popd
+else
+    git clone git@github.com:catid/leopard.git
+fi
+
+pushd leopard
+mkdir build && cd build
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    CXX=clang++ cmake -G 'Unix Makefiles' ..
+else
+    cmake -G 'Unix Makefiles' ..
+fi
+make
+popd
