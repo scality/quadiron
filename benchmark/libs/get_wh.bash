e1259054d65ada1096248c3c5fc2b5c3b3c7d469
Lam Pham-Sy <lam.pham-sy@scality.com>
2018-07-01 17:53:34 +0200

    Bench: add script get wirehair repo

---
+++ benchmark/libs/get_wh.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+#leo
+if [ -d ./wirehair/.git ]; then
+    pushd wirehair
+    git pull
+    popd
+else
+    git clone git@github.com:catid/wirehair.git
+fi
+
+pushd wirehair
+mkdir build
+pushd build
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    CXX=clang++ cmake -G 'Unix Makefiles' ..
+else
+    cmake -G 'Unix Makefiles' ..
+fi
+make
+popd
+popd
