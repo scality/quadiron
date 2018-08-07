60bfcdbc8459929d575c1ff2c8cb57252b48fb6d
Sylvain Laperche <sylvain.laperche@scality.com>
2018-08-07 08:41:12 +0200

    rewrite ArithUtest using GoogleTest
    Refs: #46

--- test/main.cpp
+++ test/main.cpp
@@ -27,56 +27,11 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include "quadiron.h"
-
-extern void arith_utest();
-extern void gf_utest();
-extern void rs_utest();
-extern void vector_utest();
-extern void buffers_utest();
-extern void mat_utest();
-extern void fft_utest();
-extern void fec_utest();
-
-struct test {
-    const char* name;
-    void (*func)();
-} tests[] = {{"arith", arith_utest},
-             {"gf", gf_utest},
-             {"vec", vector_utest},
-             {"vecp", buffers_utest},
-             {"mat", mat_utest},
-             {"rs", rs_utest},
-             {"fft", fft_utest},
-             {"fec", fec_utest},
-             {nullptr, nullptr}};
+#include <gtest/gtest.h>
 
 int main(int argc, char** argv)
 {
-    struct test* p;
-
-    quadiron::prng().seed(0);
-
-    if (2 == argc) {
-        std::stringstream ss(argv[1]);
-        std::vector<int> output;
-
-        std::string token;
-
-        while (std::getline(ss, token, ',')) {
-            for (p = tests; nullptr != p->name; p++) {
-                std::string s(p->name);
-                if (s == token) {
-                    std::cout << p->name << "\n";
-                    p->func();
-                    break;
-                }
-            }
-        }
-    } else {
-        for (p = tests; nullptr != p->name; p++) {
-            std::cout << p->name << "\n";
-            p->func();
-        }
-    }
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret = RUN_ALL_TESTS();
+    return ret;
 }
