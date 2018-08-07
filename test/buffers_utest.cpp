08495a2b5f635a3f5eae54bb9e6885fd8160a644
Sylvain Laperche <sylvain.laperche@scality.com>
2018-08-07 09:40:18 +0200

    rewrite BuffersUtest using GoogleTest
    Refs: #46

--- test/buffers_utest.cpp
+++ test/buffers_utest.cpp
@@ -29,32 +29,22 @@
  */
 #include <random>
 
+#include <gtest/gtest.h>
+
 #include "quadiron.h"
 
 template <typename T>
-class BuffersUtest {
-  private:
-    std::mt19937 prng;
-    quadiron::gf::Prime<T>* gfp;
-    T max_val;
-
+class BuffersTest : public ::testing::Test {
   public:
-    BuffersUtest()
-    {
-        this->gfp = new quadiron::gf::Prime<T>(65537);
-        this->max_val = 65537;
-    }
-    ~BuffersUtest()
-    {
-        delete this->gfp;
-    }
-
-    quadiron::vec::Buffers<T>*
+    std::unique_ptr<quadiron::vec::Buffers<T>>
     gen_buffers_rand_data(int n, int size, int _max = 0)
     {
+        std::mt19937 prng;
+        T max_val = 65537;
         const int max = (_max == 0) ? max_val : _max;
         std::uniform_int_distribution<uint32_t> dis(0, max - 1);
-        quadiron::vec::Buffers<T>* vec = new quadiron::vec::Buffers<T>(n, size);
+        auto vec = std::unique_ptr<quadiron::vec::Buffers<T>>(
+            new quadiron::vec::Buffers<T>(n, size));
 
         for (int i = 0; i < n; i++) {
             T* buf = quadiron::aligned_allocate<T>(size);
@@ -63,222 +53,160 @@ class BuffersUtest {
             }
             vec->set(i, buf);
         }
-        // vec.dump();
 
         return vec;
     }
+};
 
-    void buffers_utest1()
-    {
-        std::cout << "buffers_utest1\n";
+using TestedTypes = ::testing::Types<uint32_t, uint64_t>;
+TYPED_TEST_CASE(BuffersTest, TestedTypes);
 
-        int n = 16;
-        int begin = 5;
-        int end = 12;
-        int size = 32;
-        int i, j;
+TYPED_TEST(BuffersTest, TestConstructors) // NOLINT
+{
+    const int n = 16;
+    const int begin = 5;
+    const int end = 12;
+    const int size = 32;
 
-        quadiron::vec::Buffers<T>* vec1 = gen_buffers_rand_data(n, size);
-        std::vector<T*>* mem1 = vec1->get_mem();
-        // vec1->dump();
+    auto vec1 = this->gen_buffers_rand_data(n, size);
+    quadiron::vec::Buffers<TypeParam> vec2(vec1.get(), begin, end);
 
-        quadiron::vec::Buffers<T> vec2(vec1, begin, end);
-        std::vector<T*>* mem2 = vec2.get_mem();
-        assert(vec2.get_n() == end - begin);
-        assert(vec2.get_size() == vec1->get_size());
-        for (i = 0; i < end - begin; i++) {
-            for (j = 0; j < size; j++) {
-                mem2->at(i)[j] = mem1->at(i + begin)[j];
+    std::vector<TypeParam*>* mem1 = vec1->get_mem();
+    std::vector<TypeParam*>* mem2 = vec2.get_mem();
+
+    ASSERT_EQ(vec2.get_n(), end - begin);
+    ASSERT_EQ(vec2.get_size(), vec1->get_size());
+
+    for (int i = 0; i < end - begin; i++) {
+        for (int j = 0; j < size; j++) {
+            mem2->at(i)[j] = mem1->at(i + begin)[j];
+        }
+    }
+
+    std::vector<TypeParam*> mem3(end - begin, nullptr);
+    for (int i = 0; i < end - begin; i++) {
+        mem3[i] = mem1->at(i + begin);
+    }
+    quadiron::vec::Buffers<TypeParam> vec3(end - begin, size, &mem3);
+
+    ASSERT_TRUE(vec2.eq(&vec3));
+}
+
+TYPED_TEST(BuffersTest, TestEvenOddSeparation) // NOLINT
+{
+    const int n = 8;
+    const int size = 32;
+    const int half = n / 2;
+    auto vec1 = this->gen_buffers_rand_data(n, size);
+    quadiron::vec::Buffers<TypeParam> vec2(n, size);
+
+    vec2.copy(vec1.get());
+
+    std::vector<TypeParam*>* even_mem =
+        new std::vector<TypeParam*>(half, nullptr);
+    std::vector<TypeParam*>* odd_mem =
+        new std::vector<TypeParam*>(half, nullptr);
+    auto i_even = std::unique_ptr<quadiron::vec::Buffers<TypeParam>>(
+        new quadiron::vec::Buffers<TypeParam>(half, size, even_mem));
+    auto i_odd = std::unique_ptr<quadiron::vec::Buffers<TypeParam>>(
+        new quadiron::vec::Buffers<TypeParam>(half, size, odd_mem));
+    vec1->separate_even_odd(i_even.get(), i_odd.get());
+
+    vec1->separate_even_odd();
+
+    quadiron::vec::Buffers<TypeParam> _i_even(vec1.get(), 0, half);
+    quadiron::vec::Buffers<TypeParam> _i_odd(vec1.get(), half, n);
+    ASSERT_TRUE(i_even->eq(&_i_even));
+    ASSERT_TRUE(i_odd->eq(&_i_odd));
+
+    const std::vector<TypeParam*>* mem1 = vec1->get_mem();
+    const std::vector<TypeParam*>* mem2 = vec2.get_mem();
+
+    bool ok = true;
+    for (int i = 0; i < n / 2; i += 2) {
+        TypeParam* even1 = mem1->at(i);
+        TypeParam* even2 = mem2->at(i * 2);
+        TypeParam* odd1 = mem1->at(i + n / 2);
+        TypeParam* odd2 = mem2->at(i * 2 + 1);
+        for (int j = 0; j < size; j++) {
+            if (even1[j] != even2[j] || odd1[j] != odd2[j]) {
+                ok = false;
+                i = n;
+                break;
             }
         }
-        // vec2.dump();
-
-        std::vector<T*> mem3(end - begin, nullptr);
-        for (int i = 0; i < end - begin; i++) {
-            mem3[i] = mem1->at(i + begin);
-        }
-        quadiron::vec::Buffers<T> vec3(end - begin, size, &mem3);
-        // vec3.dump();
-
-        assert(vec2.eq(&vec3));
-
-        delete vec1;
     }
+    ASSERT_TRUE(ok);
+}
 
-    void buffers_utest2()
-    {
-        std::cout << "buffers_utest2\n";
+TYPED_TEST(BuffersTest, TestZeroExtented) // NOLINT
+{
+    const int n = 8;
+    const int size = 32;
+    const int n1 = 4;
+    const int n2 = 10;
 
-        int n = 8;
-        int size = 32;
-        int i, j;
-        int half = n / 2;
+    auto vec = this->gen_buffers_rand_data(n, size);
+    quadiron::vec::Buffers<TypeParam> vec1(vec.get(), n1);
+    quadiron::vec::Buffers<TypeParam> vec2(vec.get(), n2);
 
-        std::unique_ptr<quadiron::vec::Buffers<T>> vec1 =
-            std::unique_ptr<quadiron::vec::Buffers<T>>(
-                gen_buffers_rand_data(n, size));
-        quadiron::vec::Buffers<T> vec2(n, size);
-        vec2.copy(vec1.get());
+    quadiron::vec::Buffers<TypeParam> _vec1(vec.get(), 0, n1);
+    auto _vec2 = std::unique_ptr<quadiron::vec::Buffers<TypeParam>>(
+        new quadiron::vec::BuffersZeroExtended<TypeParam>(vec.get(), n2));
 
-        std::vector<T*>* even_mem = new std::vector<T*>(half, nullptr);
-        std::vector<T*>* odd_mem = new std::vector<T*>(half, nullptr);
-        std::unique_ptr<quadiron::vec::Buffers<T>> i_even =
-            std::unique_ptr<quadiron::vec::Buffers<T>>(
-                new quadiron::vec::Buffers<T>(half, size, even_mem));
-        std::unique_ptr<quadiron::vec::Buffers<T>> i_odd =
-            std::unique_ptr<quadiron::vec::Buffers<T>>(
-                new quadiron::vec::Buffers<T>(half, size, odd_mem));
-        vec1->separate_even_odd(i_even.get(), i_odd.get());
+    ASSERT_TRUE(vec1.eq(&_vec1));
+    ASSERT_TRUE(vec2.eq(_vec2.get()));
 
-        // vec1->dump();
-        vec1->separate_even_odd();
-        // vec1->dump();
+    quadiron::vec::Buffers<TypeParam> vec3(&vec2, n1);
+    ASSERT_TRUE(vec3.eq(&vec1));
+}
 
-        quadiron::vec::Buffers<T> _i_even(vec1.get(), 0, half);
-        quadiron::vec::Buffers<T> _i_odd(vec1.get(), half, n);
-        assert(i_even->eq(&_i_even));
-        assert(i_odd->eq(&_i_odd));
+TYPED_TEST(BuffersTest, TestPackUnpack) // NOLINT
+{
+    const int iter_count = quadiron::arith::log2<TypeParam>(sizeof(TypeParam));
 
-        // vec2.dump();
+    for (int i = 0; i <= iter_count; i++) {
+        const int word_size = quadiron::arith::exp2<TypeParam>(i);
+        const int n = 8;
+        const int size = 32;
+        const int bytes_size = size * word_size;
+        const TypeParam max = (static_cast<TypeParam>(1) << word_size) + 1;
+        auto words = this->gen_buffers_rand_data(n, size, max);
+        const std::vector<TypeParam*>* mem_T = words->get_mem();
 
-        std::vector<T*>* mem1 = vec1->get_mem();
-        std::vector<T*>* mem2 = vec2.get_mem();
-
-        bool ok = true;
-        for (i = 0; i < n / 2; i += 2) {
-            T* even1 = mem1->at(i);
-            T* even2 = mem2->at(i * 2);
-            T* odd1 = mem1->at(i + n / 2);
-            T* odd2 = mem2->at(i * 2 + 1);
-            for (j = 0; j < size; j++) {
-                if (even1[j] != even2[j] || odd1[j] != odd2[j]) {
-                    ok = false;
-                    i = n;
-                    break;
-                }
-            }
-        }
-        assert(ok);
-    }
-
-    void buffers_utest3()
-    {
-        std::cout << "buffers_utest3\n";
-
-        int n = 8;
-        int size = 32;
-        int n1 = 4;
-        int n2 = 10;
-
-        quadiron::vec::Buffers<T>* vec = gen_buffers_rand_data(n, size);
-        quadiron::vec::Buffers<T> vec1(vec, n1);
-        quadiron::vec::Buffers<T> vec2(vec, n2);
-
-        quadiron::vec::Buffers<T> _vec1(vec, 0, n1);
-        quadiron::vec::Buffers<T>* _vec2 =
-            new quadiron::vec::BuffersZeroExtended<T>(vec, n2);
-
-        assert(vec1.eq(&_vec1));
-        assert(vec2.eq(_vec2));
-
-        delete vec;
-
-        quadiron::vec::Buffers<T> vec3(&vec2, n1);
-        assert(vec3.eq(&vec1));
-
-        delete _vec2;
-    }
-
-    void buffers_utest4()
-    {
-        std::cout << "buffers_utest4\n";
-        int i;
-        int word_size;
-
-        for (i = 0; i <= quadiron::arith::log2<T>(sizeof(T)); i++) {
-            word_size = quadiron::arith::exp2<T>(i);
-            pack_unpack(word_size);
-        }
-    }
-
-    void pack_unpack(int word_size)
-    {
-        std::cout << "pack_unpack with word_size=" << word_size << "\n";
-        int n = 8;
-        int size = 32;
-        int bytes_size = size * word_size;
-        int i, j, t, u;
-
-        T symb;
-        T max = ((T)1 << word_size) + 1;
-
-        quadiron::vec::Buffers<T>* words = gen_buffers_rand_data(n, size, max);
-        std::vector<T*>* mem_T = words->get_mem();
-        // std::cout << "words:"; words->dump();
-
-        // pack manually from T to uint8_t
+        // Pack manually from TypeParam to uint8_t.
         quadiron::vec::Buffers<uint8_t> vec_char(n, bytes_size);
         std::vector<uint8_t*>* mem_char = vec_char.get_mem();
-        for (i = 0; i < n; i++) {
-            t = 0;
-            T* buf_T = mem_T->at(i);
-            uint8_t* buf_char = mem_char->at(i);
-            for (j = 0; j < size; j++) {
-                symb = buf_T[j];
-                buf_char[t] = (uint8_t)(symb & 0xFF);
+        for (int j = 0; j < n; j++) {
+            int t = 0;
+            TypeParam* buf_T = mem_T->at(j);
+            uint8_t* buf_char = mem_char->at(j);
+
+            for (int k = 0; k < size; k++) {
+                TypeParam symb = buf_T[k];
+                buf_char[t] = static_cast<uint8_t>(symb & 0xFF);
+
                 t++;
-                for (u = 1; u < word_size; u++) {
+                for (int u = 1; u < word_size; u++) {
                     symb >>= 8;
-                    buf_char[t] = (uint8_t)(symb & 0xFF);
+                    buf_char[t] = static_cast<uint8_t>(symb & 0xFF);
                     t++;
                 }
             }
         }
 
-        /*
-         * pack bufs of type uint8_t to bufs of type T
-         */
-        // tmp vectors to store results
-        quadiron::vec::Buffers<T> vec_T_tmp(n, size);
-        std::vector<T*>* mem_T_tmp = vec_T_tmp.get_mem();
-        quadiron::vec::pack<uint8_t, T>(
+        // Pack bufs of type uint8_t to bufs of type TypeParam.
+        quadiron::vec::Buffers<TypeParam> vec_T_tmp(n, size);
+        std::vector<TypeParam*>* mem_T_tmp = vec_T_tmp.get_mem();
+        quadiron::vec::pack<uint8_t, TypeParam>(
             mem_char, mem_T_tmp, n, size, word_size);
-        // std::cout << "vec_char:"; vec_char.dump();
-        // std::cout << "vec_T_tmp:"; vec_T_tmp.dump();
-        // check
-        assert(vec_T_tmp.eq(words));
+        ASSERT_TRUE(vec_T_tmp.eq(words.get()));
 
-        /*
-         * unpack bufs of type T to bufs of type uint8_t
-         */
-        // tmp vectors to store results
+        // Unpack bufs of type TypeParam to bufs of type uint8_t.
         quadiron::vec::Buffers<uint8_t> vec_char_tmp(n, bytes_size);
         std::vector<uint8_t*>* mem_char_tmp = vec_char_tmp.get_mem();
-        quadiron::vec::unpack<T, uint8_t>(
+        quadiron::vec::unpack<TypeParam, uint8_t>(
             mem_T_tmp, mem_char_tmp, n, size, word_size);
-        // std::cout << "vec_T_tmp:"; vec_T_tmp.dump();
-        // std::cout << "vec_char_tmp:"; vec_char_tmp.dump();
-        // check
-        assert(vec_char_tmp.eq(&vec_char));
-
-        delete words;
+        ASSERT_TRUE(vec_char_tmp.eq(&vec_char));
     }
-
-    void buffers_utest()
-    {
-        std::cout << "buffers_utest with sizeof(T)=" << sizeof(T) << "\n";
-
-        buffers_utest1();
-        buffers_utest2();
-        buffers_utest3();
-        buffers_utest4();
-    }
-};
-
-void buffers_utest()
-{
-    BuffersUtest<uint32_t> buffersutest_uint32;
-    buffersutest_uint32.buffers_utest();
-    BuffersUtest<uint64_t> buffersutest_uint64;
-    buffersutest_uint64.buffers_utest();
 }
