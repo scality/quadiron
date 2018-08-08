da61f3f9075b760cc3b53896b0d60d564ac6f349
Sylvain Laperche <sylvain.laperche@scality.com>
2018-08-09 00:12:00 +0200

    rewrite MatUtest using GoogleTest
    Refs: #46

--- test/mat_utest.cpp
+++ test/mat_utest.cpp
@@ -27,13 +27,13 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <gtest/gtest.h>
+
 #include "quadiron.h"
 
-void mat_utest1()
+TEST(MatrixTest, TestInvertGf11) // NOLINT
 {
-    std::cout << "mat_utest1\n";
-
-    quadiron::gf::Prime<uint32_t> gf11(11);
+    const quadiron::gf::Prime<uint32_t> gf11(11);
     quadiron::vec::Matrix<uint32_t> mat(gf11, 3, 3);
 
     mat.set(0, 0, 2);
@@ -48,22 +48,20 @@ void mat_utest1()
 
     mat.inv();
 
-    assert(mat.get(0, 0) == 8);
-    assert(mat.get(0, 1) == 6);
-    assert(mat.get(0, 2) == 1);
-    assert(mat.get(1, 0) == 7);
-    assert(mat.get(1, 1) == 9);
-    assert(mat.get(1, 2) == 10);
-    assert(mat.get(2, 0) == 0);
-    assert(mat.get(2, 1) == 6);
-    assert(mat.get(2, 2) == 5);
+    ASSERT_EQ(mat.get(0, 0), 8);
+    ASSERT_EQ(mat.get(0, 1), 6);
+    ASSERT_EQ(mat.get(0, 2), 1);
+    ASSERT_EQ(mat.get(1, 0), 7);
+    ASSERT_EQ(mat.get(1, 1), 9);
+    ASSERT_EQ(mat.get(1, 2), 10);
+    ASSERT_EQ(mat.get(2, 0), 0);
+    ASSERT_EQ(mat.get(2, 1), 6);
+    ASSERT_EQ(mat.get(2, 2), 5);
 }
 
-void mat_utest2()
+TEST(MatrixTest, TestInvertGf29) // NOLINT
 {
-    std::cout << "mat_utest2\n";
-
-    quadiron::gf::Prime<uint32_t> gf29(29);
+    const quadiron::gf::Prime<uint32_t> gf29(29);
     quadiron::vec::Matrix<uint32_t> mat(gf29, 3, 3);
 
     mat.set(0, 0, 22);
@@ -78,21 +76,13 @@ void mat_utest2()
 
     mat.inv();
 
-    assert(mat.get(0, 0) == 1);
-    assert(mat.get(0, 1) == 18);
-    assert(mat.get(0, 2) == 8);
-    assert(mat.get(1, 0) == 2);
-    assert(mat.get(1, 1) == 8);
-    assert(mat.get(1, 2) == 11);
-    assert(mat.get(2, 0) == 20);
-    assert(mat.get(2, 1) == 24);
-    assert(mat.get(2, 2) == 14);
-}
-
-void mat_utest()
-{
-    std::cout << "mat_utest\n";
-
-    mat_utest1();
-    mat_utest2();
+    ASSERT_EQ(mat.get(0, 0), 1);
+    ASSERT_EQ(mat.get(0, 1), 18);
+    ASSERT_EQ(mat.get(0, 2), 8);
+    ASSERT_EQ(mat.get(1, 0), 2);
+    ASSERT_EQ(mat.get(1, 1), 8);
+    ASSERT_EQ(mat.get(1, 2), 11);
+    ASSERT_EQ(mat.get(2, 0), 20);
+    ASSERT_EQ(mat.get(2, 1), 24);
+    ASSERT_EQ(mat.get(2, 2), 14);
 }
