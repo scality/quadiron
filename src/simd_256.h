a3f984348cb365ae4ab63623c88981ed01765b31
Lam Pham-Sy <lam.pham-sy@scality.com>
2018-10-05 13:44:16 +0200

    SIMD 256: essential operations for AVX

--- src/simd_256.h
+++ src/simd_256.h
@@ -33,19 +33,6 @@
 
 #include <x86intrin.h>
 
-typedef __m256i m256i;
-
-// Disable `cert-err58-cpp` on these: AFAIK they cannot throw.
-// (probably a false positive present in Clang 5 and fixed in Clang 6).
-const m256i F4_m256i = _mm256_set1_epi32(65537);       // NOLINT(cert-err58-cpp)
-const m256i F4minus1_m256i = _mm256_set1_epi32(65536); // NOLINT(cert-err58-cpp)
-const m256i F3_m256i = _mm256_set1_epi32(257);         // NOLINT(cert-err58-cpp)
-const m256i F3minus1_m256i = _mm256_set1_epi32(256);   // NOLINT(cert-err58-cpp)
-
-const m256i F3_m256i_u16 = _mm256_set1_epi16(257); // NOLINT(cert-err58-cpp)
-// NOLINTNEXTLINE(cert-err58-cpp)
-const m256i F3minus1_m256i_u16 = _mm256_set1_epi16(256);
-
 /* GCC doesn't include the split store intrinsics so define them here. */
 #if defined(__GNUC__) && !defined(__clang__)
 
@@ -58,7 +45,145 @@ _mm256_storeu2_m128i(__m128i* const hi, __m128i* const lo, const __m256i a)
 
 #endif /* defined(__GNUC__) */
 
-#include "simd_256_u16.h"
-#include "simd_256_u32.h"
+namespace quadiron {
+namespace simd {
+
+typedef __m256i VecType;
+typedef __m128i HalfVecType;
+
+/* ============= Constant variable  ============ */
+
+#define F4_U32 _mm256_set1_epi32(65537)
+#define F4_MINUS_ONE_U32 _mm256_set1_epi32(65536)
+#define F3_U32 _mm256_set1_epi32(257)
+#define F3_MINUS_ONE_U32 _mm256_set1_epi32(256)
+
+#define F3_U16 _mm256_set1_epi16(257)
+#define F3_MINUS_ONE_U16 _mm256_set1_epi16(256)
+
+#define ZERO (_mm256_setzero_si256())
+#define ONE_U16 (_mm256_set1_epi16(1))
+#define ONE_U32 (_mm256_set1_epi32(1))
+
+#define MASK8_LO (_mm256_set1_epi16(0x80))
+
+/* ============= Essential Operations for AVX2 w/ both u16 & u32 ============ */
+
+inline VecType load_to_reg(VecType* address)
+{
+    return _mm256_load_si256(address);
+}
+inline void store_to_mem(VecType* address, VecType reg)
+{
+    _mm256_store_si256(address, reg);
+}
+
+inline VecType bit_and(VecType x, VecType y)
+{
+    return _mm256_and_si256(x, y);
+}
+inline VecType bit_xor(VecType x, VecType y)
+{
+    return _mm256_xor_si256(x, y);
+}
+inline uint32_t msb8_mask(VecType x)
+{
+    return _mm256_movemask_epi8(x);
+}
+inline bool and_is_zero(VecType x, VecType y)
+{
+    return _mm256_testz_si256(x, y);
+}
+inline bool is_zero(VecType x)
+{
+    return _mm256_testc_si256(ZERO, x);
+}
+
+#define SHIFTR(x, imm8) (_mm256_srli_si256(x, imm8))
+#define BLEND8(x, y, mask) (_mm256_blendv_epi8(x, y, mask))
+#define BLEND16(x, y, imm8) (_mm256_blend_epi16(x, y, imm8))
+
+/* ================= Essential Operations for AVX2 ================= */
+
+template <typename T>
+inline VecType set_one(T val);
+template <>
+inline VecType set_one(uint32_t val)
+{
+    return _mm256_set1_epi32(val);
+}
+template <>
+inline VecType set_one(uint16_t val)
+{
+    return _mm256_set1_epi16(val);
+}
+
+template <typename T>
+inline VecType add(VecType x, VecType y);
+template <>
+inline VecType add<uint32_t>(VecType x, VecType y)
+{
+    return _mm256_add_epi32(x, y);
+}
+template <>
+inline VecType add<uint16_t>(VecType x, VecType y)
+{
+    return _mm256_add_epi16(x, y);
+}
+
+template <typename T>
+inline VecType sub(VecType x, VecType y);
+template <>
+inline VecType sub<uint32_t>(VecType x, VecType y)
+{
+    return _mm256_sub_epi32(x, y);
+}
+template <>
+inline VecType sub<uint16_t>(VecType x, VecType y)
+{
+    return _mm256_sub_epi16(x, y);
+}
+
+template <typename T>
+inline VecType mul(VecType x, VecType y);
+template <>
+inline VecType mul<uint32_t>(VecType x, VecType y)
+{
+    return _mm256_mullo_epi32(x, y);
+}
+template <>
+inline VecType mul<uint16_t>(VecType x, VecType y)
+{
+    return _mm256_mullo_epi16(x, y);
+}
+
+template <typename T>
+inline VecType compare_eq(VecType x, VecType y);
+template <>
+inline VecType compare_eq<uint32_t>(VecType x, VecType y)
+{
+    return _mm256_cmpeq_epi32(x, y);
+}
+template <>
+inline VecType compare_eq<uint16_t>(VecType x, VecType y)
+{
+    return _mm256_cmpeq_epi16(x, y);
+}
+
+template <typename T>
+inline VecType min(VecType x, VecType y);
+template <>
+inline VecType min<uint32_t>(VecType x, VecType y)
+{
+    return _mm256_min_epu32(x, y);
+}
+template <>
+inline VecType min<uint16_t>(VecType x, VecType y)
+{
+    return _mm256_min_epu16(x, y);
+}
+
+} // namespace simd
+} // namespace quadiron
 
 #endif
