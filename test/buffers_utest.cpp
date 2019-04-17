61712080253639f70c21b2f85892a7b32e0ac53d
Lam Pham-Sy <lam.pham-sy@scality.com>
2019-04-17 15:02:30 +0200

    Update Buffers' test for meta and support uint8_t and uint16_t

--- test/buffers_utest.cpp
+++ test/buffers_utest.cpp
@@ -38,9 +38,21 @@ namespace vec = quadiron::vec;
 namespace gf = quadiron::gf;
 
 template <typename T>
+bool is_all_zeros(const T* buf, size_t len)
+{
+    for (size_t i = 0; i < len; ++i) {
+        if (buf[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
 class BuffersTest : public ::testing::Test {
   public:
     quadiron::simd::AlignedAllocator<T> allocator;
+    quadiron::simd::AlignedAllocator<uint8_t> allocator_meta;
 
     BuffersTest()
     {
@@ -49,19 +61,27 @@ class BuffersTest : public ::testing::Test {
     ~BuffersTest() = default;
 
     std::unique_ptr<vec::Buffers<T>>
-    gen_buffers_rand_data(int n, int size, int _max = 0)
+    gen_buffers_rand_data(int n, int size, bool has_meta = false, int _max = 0)
     {
-        T max_val = 65537;
-        const int max = (_max == 0) ? max_val : _max;
-        std::uniform_int_distribution<uint32_t> dis(0, max - 1);
-        auto vec = std::make_unique<vec::Buffers<T>>(n, size);
+        const T max = (_max == 0) ? std::numeric_limits<T>::max() : _max;
+        std::uniform_int_distribution<T> dis(0, max - 1);
+        auto vec = std::make_unique<vec::Buffers<T>>(n, size, has_meta);
 
+        const std::vector<T*> mem = vec->get_mem();
         for (int i = 0; i < n; i++) {
-            T* buf = this->allocator.allocate(size);
             for (int j = 0; j < size; j++) {
-                buf[j] = dis(quadiron::prng());
+                mem[i][j] = dis(quadiron::prng());
             }
-            vec->set(i, buf);
+        }
+
+        if (has_meta) {
+            const std::vector<uint8_t*> meta = vec->get_meta();
+            const size_t meta_size = vec->get_meta_size();
+            for (int i = 0; i < n; ++i) {
+                for (size_t j = 0; j < meta_size; ++j) {
+                    meta[i][j] = static_cast<uint8_t>(dis(quadiron::prng()));
+                }
+            }
         }
 
         return vec;
@@ -85,21 +105,6 @@ class BuffersTest : public ::testing::Test {
         return vec;
     }
 
-    bool check_eq(const T* buf1, const T* buf2, size_t len)
-    {
-        return memcmp(buf1, buf2, len * sizeof(T)) == 0;
-    }
-
-    bool check_all_zeros(const T* buf, size_t len)
-    {
-        for (size_t i = 0; i < len; ++i) {
-            if (buf[i] != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     bool check_shuffled_bufs(
         const vec::Buffers<T>& input,
         const vec::Buffers<T>& output,
@@ -114,7 +119,17 @@ class BuffersTest : public ::testing::Test {
         std::vector<bool> check(output.get_n(), false);
 
         for (unsigned i = 0; i < map_len; ++i) {
-            if (!check_eq(input.get(i), output.get(map.get(i)), size)) {
+            if (!std::equal(
+                    input.get(i),
+                    input.get(i) + size,
+                    output.get(map.get(i)))) {
+                return false;
+            }
+            if (input.has_meta()
+                && !std::equal(
+                       input.get_meta(i),
+                       input.get_meta(i) + input.get_meta_size(),
+                       output.get_meta(map.get(i)))) {
                 return false;
             }
             check[map.get(i)] = true;
@@ -124,7 +139,12 @@ class BuffersTest : public ::testing::Test {
         if (output_len > input_len) {
             for (size_t i = 0; i < output_len; ++i) {
                 if (!check[i]) {
-                    if (!check_all_zeros(output.get(i), size)) {
+                    if (!is_all_zeros(output.get(i), size)) {
+                        return false;
+                    }
+                    if (input.has_meta()
+                        && !is_all_zeros(
+                               output.get_meta(i), output.get_meta_size())) {
                         return false;
                     }
                 }
@@ -135,97 +155,88 @@ class BuffersTest : public ::testing::Test {
     }
 };
 
-using TestedTypes = ::testing::Types<uint32_t, uint64_t>;
+using TestedTypes = ::testing::Types<uint8_t, uint16_t, uint32_t, uint64_t>;
 TYPED_TEST_CASE(BuffersTest, TestedTypes);
 
 TYPED_TEST(BuffersTest, TestConstructors) // NOLINT
 {
-    const int n = 16;
-    const int begin = 5;
-    const int end = 12;
-    const int size = 4;
+    const std::vector<bool> tests = {true, false};
 
-    auto vec1 = this->gen_buffers_rand_data(n, size);
-    vec::Buffers<TypeParam> vec2(*vec1, begin, end);
+    for (bool const& has_meta : tests) {
+        const int n = 16;
+        const int begin = 5;
+        const int end = 12;
+        const int size = 4;
 
-    const std::vector<TypeParam*> mem1 = vec1->get_mem();
-    const std::vector<TypeParam*> mem2 = vec2.get_mem();
+        auto vec1 = this->gen_buffers_rand_data(n, size, has_meta);
+        ASSERT_EQ(has_meta, vec1->has_meta());
 
-    ASSERT_EQ(vec2.get_n(), end - begin);
-    ASSERT_EQ(vec2.get_size(), vec1->get_size());
+        const size_t meta_size = vec1->get_meta_size();
 
-    for (int i = 0; i < end - begin; i++) {
-        for (int j = 0; j < size; j++) {
-            mem2.at(i)[j] = mem1.at(i + begin)[j];
+        vec::Buffers<TypeParam> vec2(*vec1, begin, end);
+
+        const std::vector<TypeParam*> mem1 = vec1->get_mem();
+        const std::vector<TypeParam*> mem2 = vec2.get_mem();
+
+        // Check Slice constructor
+        ASSERT_EQ(vec2.get_n(), end - begin);
+        ASSERT_EQ(vec2.get_size(), vec1->get_size());
+        for (int i = begin, j = 0; i < end; ++i, ++j) {
+            ASSERT_TRUE(std::equal(mem1[i], mem1[i] + size, mem2[j]));
         }
-    }
 
-    std::vector<TypeParam*> mem3(end - begin, nullptr);
-    for (int i = 0; i < end - begin; i++) {
-        mem3[i] = mem1.at(i + begin);
-    }
-    vec::Buffers<TypeParam> vec3(end - begin, size, mem3);
+        if (has_meta) {
+            ASSERT_EQ(vec2.has_meta(), vec1->has_meta());
+            ASSERT_EQ(vec2.get_meta_size(), meta_size);
 
-    ASSERT_EQ(vec2, vec3);
+            const std::vector<uint8_t*> meta1 = vec1->get_meta();
+            const std::vector<uint8_t*> meta2 = vec2.get_meta();
 
-    auto gf(gf::create<gf::Prime<TypeParam>>(65537));
-
-    // no-extension
-    const int out_n_1 = n - 5;
-    auto map_1 = this->gen_rand_vector(gf, out_n_1, out_n_1);
-    vec::Buffers<TypeParam> vec4(*vec1, *map_1, out_n_1);
-    ASSERT_TRUE(this->check_shuffled_bufs(*vec1, vec4, *map_1));
-
-    // extension
-    const int out_n_2 = n + 10;
-    auto map_2 = this->gen_rand_vector(gf, n, out_n_2);
-    vec::Buffers<TypeParam> vec5(*vec1, *map_2, out_n_2);
-    ASSERT_TRUE(this->check_shuffled_bufs(*vec1, vec5, *map_2));
-}
-
-TYPED_TEST(BuffersTest, TestEvenOddSeparation) // NOLINT
-{
-    const int n = 8;
-    const int size = 32;
-    const int half = n / 2;
-    auto vec1 = this->gen_buffers_rand_data(n, size);
-    vec::Buffers<TypeParam> vec2(n, size);
-
-    vec2.copy(*vec1);
-
-    std::vector<TypeParam*>* even_mem =
-        new std::vector<TypeParam*>(half, nullptr);
-    std::vector<TypeParam*>* odd_mem =
-        new std::vector<TypeParam*>(half, nullptr);
-    vec::Buffers<TypeParam> i_even(half, size, *even_mem);
-    vec::Buffers<TypeParam> i_odd(half, size, *odd_mem);
-    vec1->separate_even_odd(i_even, i_odd);
-
-    vec1->separate_even_odd();
-
-    vec::Buffers<TypeParam> _i_even(*vec1, 0, half);
-    vec::Buffers<TypeParam> _i_odd(*vec1, half, n);
-    ASSERT_EQ(i_even, _i_even);
-    ASSERT_EQ(i_odd, _i_odd);
-
-    const std::vector<TypeParam*> mem1 = vec1->get_mem();
-    const std::vector<TypeParam*> mem2 = vec2.get_mem();
-
-    bool ok = true;
-    for (int i = 0; i < n / 2; i += 2) {
-        TypeParam* even1 = mem1.at(i);
-        TypeParam* even2 = mem2.at(i * 2);
-        TypeParam* odd1 = mem1.at(i + n / 2);
-        TypeParam* odd2 = mem2.at(i * 2 + 1);
-        for (int j = 0; j < size; j++) {
-            if (even1[j] != even2[j] || odd1[j] != odd2[j]) {
-                ok = false;
-                i = n;
-                break;
+            for (int i = begin, j = 0; i < end; ++i, ++j) {
+                ASSERT_TRUE(
+                    std::equal(meta1[i], meta1[i] + meta_size, meta2[j]));
             }
         }
+
+        // Check constructor from given mem and meta
+        std::vector<TypeParam*> mem3(end - begin);
+        for (int i = 0; i < end - begin; i++) {
+            mem3[i] = this->allocator.allocate(size);
+            std::copy_n(mem1[i + begin], size, mem3[i]);
+        }
+
+        if (has_meta) {
+            const std::vector<uint8_t*> meta1 = vec1->get_meta();
+            std::vector<uint8_t*> meta3(end - begin);
+            for (int i = 0; i < end - begin; i++) {
+                meta3[i] = this->allocator_meta.allocate(meta_size);
+                std::copy_n(meta1[i + begin], meta_size, meta3[i]);
+            }
+
+            vec::Buffers<TypeParam> vec3(end - begin, size, mem3, &meta3);
+
+            ASSERT_EQ(vec2, vec3);
+
+        } else {
+            vec::Buffers<TypeParam> vec3(end - begin, size, mem3);
+
+            ASSERT_EQ(vec2, vec3);
+        }
+
+        auto gf(gf::create<gf::Prime<TypeParam>>(static_cast<TypeParam>(31)));
+
+        // no-extension
+        const int out_n_1 = n - 5;
+        auto map_1 = this->gen_rand_vector(gf, out_n_1, out_n_1);
+        vec::Buffers<TypeParam> vec4(*vec1, *map_1, out_n_1);
+        ASSERT_TRUE(this->check_shuffled_bufs(*vec1, vec4, *map_1));
+
+        // extension
+        const int out_n_2 = n + 10;
+        auto map_2 = this->gen_rand_vector(gf, n, out_n_2);
+        vec::Buffers<TypeParam> vec5(*vec1, *map_2, out_n_2);
+        ASSERT_TRUE(this->check_shuffled_bufs(*vec1, vec5, *map_2));
     }
-    ASSERT_TRUE(ok);
 }
 
 TYPED_TEST(BuffersTest, TestZeroExtented) // NOLINT
@@ -235,23 +246,32 @@ TYPED_TEST(BuffersTest, TestZeroExtented) // NOLINT
     const int n1 = 4;
     const int n2 = 10;
 
-    auto vec = this->gen_buffers_rand_data(n, size);
-    vec::Buffers<TypeParam> vec1(*vec, n1);
-    vec::Buffers<TypeParam> vec2(*vec, n2);
+    const std::vector<bool> tests = {true, false};
 
-    vec::Buffers<TypeParam> _vec1(*vec, 0, n1);
-    vec::Buffers<TypeParam> _vec2(*vec, n2);
+    for (bool const& has_meta : tests) {
+        auto vec = this->gen_buffers_rand_data(n, size, has_meta);
+        // cloned constructor: no zero-padding as `n1 < n`
+        vec::Buffers<TypeParam> vec1(*vec, n1);
+        // cloned constructor: there are zero-padding as `n2 > n`
+        vec::Buffers<TypeParam> vec2(*vec, n2);
 
-    ASSERT_EQ(vec1, _vec1);
-    ASSERT_EQ(vec2, _vec2);
+        // slice from `vec` as `n1 < n`
+        vec::Buffers<TypeParam> _vec1(*vec, 0, n1);
+        // slice and zero-extended from `vec` as `n2 > n`
+        vec::Buffers<TypeParam> _vec2(*vec, 0, n2);
 
-    vec::Buffers<TypeParam> vec3(vec2, n1);
-    ASSERT_EQ(vec3, vec1);
+        ASSERT_EQ(vec1, _vec1);
+        ASSERT_EQ(vec2, _vec2);
+
+        vec::Buffers<TypeParam> vec3(vec2, n);
+        ASSERT_EQ(vec3, *vec);
+    }
 }
 
 TYPED_TEST(BuffersTest, TestPackUnpack) // NOLINT
 {
     const int iter_count = quadiron::arith::log2<TypeParam>(sizeof(TypeParam));
+    const std::vector<bool> tests = {true, false};
 
     for (int i = 0; i <= iter_count; i++) {
         const int word_size = quadiron::arith::exp2<TypeParam>(i);
@@ -259,41 +279,54 @@ TYPED_TEST(BuffersTest, TestPackUnpack) // NOLINT
         const int size = 32;
         const int bytes_size = size * word_size;
         const TypeParam max = (static_cast<TypeParam>(1) << word_size) + 1;
-        auto words = this->gen_buffers_rand_data(n, size, max);
-        const std::vector<TypeParam*> mem_T = words->get_mem();
 
-        // Pack manually from TypeParam to uint8_t.
-        vec::Buffers<uint8_t> vec_char(n, bytes_size);
-        const std::vector<uint8_t*> mem_char = vec_char.get_mem();
-        for (int j = 0; j < n; j++) {
-            int t = 0;
-            TypeParam* buf_T = mem_T.at(j);
-            uint8_t* buf_char = mem_char.at(j);
+        for (bool const& has_meta : tests) {
+            auto words = this->gen_buffers_rand_data(n, size, has_meta, max);
 
-            for (int k = 0; k < size; k++) {
-                TypeParam symb = buf_T[k];
-                buf_char[t] = static_cast<uint8_t>(symb & 0xFF);
+            const std::vector<TypeParam*> mem_T = words->get_mem();
 
-                t++;
-                for (int u = 1; u < word_size; u++) {
-                    symb >>= 8;
+            // Pack manually from TypeParam to uint8_t.
+            vec::Buffers<uint8_t> vec_char(n, bytes_size);
+            const std::vector<uint8_t*> mem_char = vec_char.get_mem();
+            for (int j = 0; j < n; j++) {
+                int t = 0;
+                TypeParam* buf_T = mem_T.at(j);
+                uint8_t* buf_char = mem_char.at(j);
+
+                for (int k = 0; k < size; k++) {
+                    TypeParam symb = buf_T[k];
                     buf_char[t] = static_cast<uint8_t>(symb & 0xFF);
+
                     t++;
+                    for (int u = 1; u < word_size; u++) {
+                        symb = static_cast<TypeParam>(symb) >> 8;
+                        buf_char[t] = static_cast<uint8_t>(symb & 0xFF);
+                        t++;
+                    }
                 }
             }
+
+            // Pack bufs of type uint8_t to bufs of type TypeParam.
+            vec::Buffers<TypeParam> vec_T_tmp(n, size);
+            const std::vector<TypeParam*> mem_T_tmp = vec_T_tmp.get_mem();
+
+            vec::pack<uint8_t, TypeParam>(
+                mem_char, mem_T_tmp, n, size, word_size);
+
+            // Unpack bufs of type TypeParam to bufs of type uint8_t.
+            vec::Buffers<uint8_t> vec_char_tmp(n, bytes_size);
+            const std::vector<uint8_t*> mem_char_tmp = vec_char_tmp.get_mem();
+            vec::unpack<TypeParam, uint8_t>(
+                mem_T_tmp, mem_char_tmp, n, size, word_size);
+
+            for (int i = 0; i < n; ++i) {
+                ASSERT_TRUE(
+                    std::equal(mem_T_tmp[i], mem_T_tmp[i] + size, mem_T[i]));
+                ASSERT_TRUE(std::equal(
+                    mem_char_tmp[i],
+                    mem_char_tmp[i] + bytes_size,
+                    mem_char[i]));
+            }
         }
-
-        // Pack bufs of type uint8_t to bufs of type TypeParam.
-        vec::Buffers<TypeParam> vec_T_tmp(n, size);
-        const std::vector<TypeParam*> mem_T_tmp = vec_T_tmp.get_mem();
-        vec::pack<uint8_t, TypeParam>(mem_char, mem_T_tmp, n, size, word_size);
-        ASSERT_EQ(vec_T_tmp, *words);
-
-        // Unpack bufs of type TypeParam to bufs of type uint8_t.
-        vec::Buffers<uint8_t> vec_char_tmp(n, bytes_size);
-        const std::vector<uint8_t*> mem_char_tmp = vec_char_tmp.get_mem();
-        vec::unpack<TypeParam, uint8_t>(
-            mem_T_tmp, mem_char_tmp, n, size, word_size);
-        ASSERT_EQ(vec_char_tmp, vec_char);
     }
 }
