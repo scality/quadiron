3ff8bbe5b6a7af48677e56cfeee7a2f209c52d3d
Lam Pham-Sy <lam.pham-sy@scality.com>
2019-04-25 17:20:35 +0200

    Fec utest: update & add test for vertical fec

--- test/fec_utest.cpp
+++ test/fec_utest.cpp
@@ -40,9 +40,12 @@ class FecTestCommon : public ::testing::Test {
   public:
     const unsigned n_data = 3;
     const unsigned n_parities = 3;
+    const size_t pkt_size = 16;
 
-    void
-    run_test(fec::FecCode<T>& fec, bool props_flag = false, bool is_nf4 = false)
+    void run_test_horizontal(
+        fec::FecCode<T>& fec,
+        bool props_flag = false,
+        bool is_nf4 = false)
     {
         const int code_len = n_data + n_parities;
 
@@ -91,6 +94,87 @@ class FecTestCommon : public ::testing::Test {
             fec.decode(*context, decoded_frags, props, 0, received_frags);
 
             ASSERT_EQ(copied_data_frags, decoded_frags);
+        }
+    }
+
+    void run_test_vertical(
+        fec::FecCode<T>& fec,
+        bool props_flag = false,
+        bool is_nf4 = false,
+        bool has_meta = false)
+    {
+        const int code_len = n_data + n_parities;
+
+        const quadiron::gf::Field<T>& gf = fec.get_gf();
+        const quadiron::gf::NF4<T>& nf4 =
+            static_cast<const quadiron::gf::NF4<T>&>(gf);
+
+        vec::Buffers<T> data_frags(n_data, pkt_size, has_meta);
+        vec::Buffers<T> copied_data_frags(n_data, pkt_size, has_meta);
+        vec::Buffers<T> encoded_frags(fec.n, pkt_size, has_meta);
+        vec::Buffers<T> received_frags(n_data, pkt_size, has_meta);
+        vec::Buffers<T> decoded_frags(n_data, pkt_size, has_meta);
+        std::vector<int> ids;
+        vec::Vector<T> fragments_ids(gf, n_data);
+
+        for (int i = 0; i < code_len; i++) {
+            ids.push_back(i);
+        }
+
+        std::vector<quadiron::Properties> props(code_len);
+        for (int j = 0; j < 1000; j++) {
+            if (props_flag) {
+                for (int i = 0; i < code_len; i++) {
+                    props[i] = quadiron::Properties();
+                }
+            }
+
+            const std::vector<T*> mem = data_frags.get_mem();
+            for (unsigned i = 0; i < n_data; i++) {
+                for (size_t j = 0; j < pkt_size; ++j) {
+                    if (has_meta) {
+                        const T hi = is_nf4 ? nf4.unpacked_rand() : gf.rand();
+                        const T lo = is_nf4 ? nf4.unpacked_rand() : gf.rand();
+                        data_frags.set(i, j, hi, lo);
+                    } else {
+                        mem[i][j] = is_nf4 ? nf4.unpacked_rand() : gf.rand();
+                    }
+                }
+            }
+            if (has_meta) {
+                data_frags.reset_meta();
+            }
+
+            // FIXME: ngff4 will modify v after encode
+            copied_data_frags.copy(data_frags);
+
+            fec.encode(encoded_frags, props, 0, data_frags);
+
+            std::random_shuffle(ids.begin(), ids.end());
+            for (unsigned i = 0; i < n_data; i++) {
+                fragments_ids.set(i, ids.at(i));
+                received_frags.copy(encoded_frags, ids.at(i), i);
+            }
+            std::unique_ptr<fec::DecodeContext<T>> context =
+                fec.init_context_dec(
+                    fragments_ids, props, pkt_size, &decoded_frags);
+
+            fec.decode(*context, decoded_frags, props, 0, received_frags);
+
+            ASSERT_EQ(copied_data_frags, decoded_frags);
+        }
+    }
+
+    void run_test(
+        fec::FecCode<T>& fec,
+        bool props_flag = false,
+        bool is_nf4 = false,
+        size_t pkt_size = 0,
+        bool has_meta = false)
+    {
+        run_test_horizontal(fec, props_flag, is_nf4);
+        if (pkt_size > 0) {
+            run_test_vertical(fec, props_flag, is_nf4, has_meta);
         }
     }
 };
@@ -143,8 +227,9 @@ TYPED_TEST(FecTestNo128, TestFnt) // NOLINT
             fec::FecType::NON_SYSTEMATIC,
             word_size,
             this->n_data,
-            this->n_parities);
-        this->run_test(fec, true);
+            this->n_parities,
+            this->pkt_size);
+        this->run_test(fec, true, false, this->pkt_size, true);
     }
 }
 
@@ -155,8 +240,9 @@ TYPED_TEST(FecTestNo128, TestFntSys) // NOLINT
             fec::FecType::SYSTEMATIC,
             word_size,
             this->n_data,
-            this->n_parities);
-        this->run_test(fec, true);
+            this->n_parities,
+            this->pkt_size);
+        this->run_test(fec, true, false);
     }
 }
 
